8bd09025479f45fc9e887e8e1fe03ff50942fff0
jonathan <jon@thanlocke.com>
2021-06-04 10:38:33 -0700

    🚧 Work on mesakit build

---
+++ mesakit-map-examples/src/main/java/com/telenav/mesakit/examples/map/measurements/LocationExample.java
@@ -0,0 +1,17 @@
+package com.telenav.mesakit.examples.map.measurements;
+
+import com.telenav.kivakit.application.Application;
+
+public class LocationExample extends Application
+{
+    public static void main(final String[] arguments)
+    {
+        new LocationExample().run(arguments);
+    }
+
+    @Override
+    protected void onRun()
+    {
+
+    }
+}
---
+++ mesakit-map-examples/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+open module mesakit.map.examples
+{
+    requires transitive kivakit.application;
+    requires transitive mesakit.map.measurements;
+
+    exports com.telenav.mesakit.examples.map.measurements;
+}
