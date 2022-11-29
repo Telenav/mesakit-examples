5cfd938d237162f4043555c6fa6b97e8222d7f56
Jonathan Locke <jonathanl@telenav.com>
2022-11-29 12:12:51 -0700

    Bugfix/demodularize (#7)
    * 🚧 work in progress on demodularizing kivakit
    
    * 🐛 Fix remaining tests
    
    * 👷 Update javadoc paths for antlr4

--- mesakit-map-examples/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-open module mesakit.map.examples
-{
-    requires transitive kivakit.application;
-    requires transitive mesakit.map.measurements;
-
-    exports com.telenav.mesakit.examples.map.measurements;
-}
